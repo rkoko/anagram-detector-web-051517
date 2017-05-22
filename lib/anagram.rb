7cc1ac9fb467927a6984c082c7493436cf942a94
rkoko <rinda.ko@gmail.com>
2017-05-21 21:09:06 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(anagram_array)
+  #  binding.pry
+    anagrams=[]
+    anagram_array.each do |anagram|
+      char_array = anagram_array.collect  {|anagram| anagram.chars}
+        char_array.each do |char|
+          if char.sort == @word.chars.sort
+            anagrams<<char.join
+          end
+        end
+          return anagrams
+      end
+
+end
+end
